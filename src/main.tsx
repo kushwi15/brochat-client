512ab04f379920d26860c6cd107a9343be04dbca
Kushwinth <kushwinthkumar0215@gmail.com>
2026-05-01 23:20:44 +0530

    feat: implemented Google OAuth authentication on LoginPage and configure environment variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App'
 
+// Get Client ID from environment variables
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
 )
